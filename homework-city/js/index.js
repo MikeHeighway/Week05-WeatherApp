4dcbb374e6f6d35d522abb8273ed1d405fe3b0d3
Mike Heighway <lineweight@gmail.com>
2016-07-09 14:32:09 -0600

    breaking things

--- homework-city/js/index.js
+++ homework-city/js/index.js
@@ -1,14 +1,24 @@
 // Pseudocode
 // -----------------------
 // Prevent default form submission
-// User enters city name in form field
-// User enters pre-selected text (i.e. NYC, New York, New York City)
-// User submits city name through button click
-// City image in body is replaced by appropriately named file
-// 
+// Page loads with default image
+// User enters pre-selected city (i.e. NYC, New York, New York City)
+// User submits city name through "Submit" button click
+// Read the name in input box
+// Replace background image with associated name
+// Button targets background city image in <body>
+// Background is changed to appropriate city image
 
 
 //Prevent Default
 $( "#mainform" ).submit(function( event ) {
   event.preventDefault();
+});
+
+
+//Change Image
+$("#submit-btn").click(function( event ){
+	// Test to see if button works 
+	// alert("Hello");
+	$("body").html("background", url("img/nyc.jpg");
 });
